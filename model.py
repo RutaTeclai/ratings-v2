7cb4e7e3ccf7bd33d73593e2b8da2db3c9fe887f
RutaTeclai <rutisyear@gmail.com>
2020-10-22 21:04:14 +0000

    Create table ratings (rating_id, score,(moive_id,  and user_id as fk), create table movies
    Co-authored-by: EssmaI <essma@outlook.com>

--- model.py
+++ model.py
@@ -21,6 +21,38 @@ class User(db.Model):
     def __repr__(self):
         return f'<User user_id={self.user_id} email={self.email}>'
 
+
+class Movie(db.Model):
+    """A user."""
+
+    __tablename__ = 'movies'
+
+    movie_id = db.Column(db.Integer,
+                        autoincrement= True,
+                        primary_key= True)
+    title = db.Column(db.String)
+    overview = db.Column(db.Text)
+    release_date = db.Column(db.DateTime)
+    poster_path = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Movie movie_id={self.movie_id} title={self.title}>'
+
+class Rating(db.Model):
+    """A user."""
+
+    __tablename__ = 'ratings'
+
+    rating_id = db.Column(db.Integer,
+                        autoincrement= True,
+                        primary_key= True)
+    score = db.Column(db.Integer)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+    def __repr__(self):
+        return f'<Rating  score={self.score} rating_id={self.rating_id}>'
+
 def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     flask_app.config['SQLALCHEMY_ECHO'] = echo
