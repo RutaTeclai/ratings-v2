b743e41c7939eff147174f83ea7a8286fccb3113
RutaTeclai <rutisyear@gmail.com>
2020-10-23 19:25:07 +0000

    define creat_movie(title, overview, release_date, poster_path) and add to database
    Co-authored-by: EssmaI <essma@outlook.com>

--- crud.py
+++ crud.py
@@ -1,7 +1,7 @@
 """CRUD operations."""
 
 from model import db, User, Movie, Rating, connect_to_db
-
+from datetime import datetime
 
 # Functions start here!
 
@@ -15,6 +15,15 @@ def create_user(email, password):
 
     return user
 
+def create_movie(title, overview, release_date, poster_path):
+    """Create and return a new movie."""
+
+    movie = Movie(title=title, overview = overview,
+                     release_date= release_date, poster_path = poster_path)
+    db.session.add(movie)
+    db.session.commit()
+
+    return movie
 
 
 if __name__ == '__main__':
